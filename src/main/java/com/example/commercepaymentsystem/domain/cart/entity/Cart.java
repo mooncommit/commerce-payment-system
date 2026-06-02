c82de5f353006c450d0d263f3a5d022e744dcb0c
Donghee Lee <dhlee8461@gmail.com>
2026-06-02 19:12:38 +0900

    feat: 상품, 장바구니 엔티티 설계

---
+++ src/main/java/com/example/commercepaymentsystem/domain/cart/entity/Cart.java
@@ -0,0 +1,29 @@
+package com.example.commercepaymentsystem.domain.cart.entity;
+
+import com.example.commercepaymentsystem.domain.member.entity.Member;
+import com.example.commercepaymentsystem.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "cart")
+public class Cart extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItems = new ArrayList<>();
+}
---
+++ src/main/java/com/example/commercepaymentsystem/domain/cart/entity/CartItem.java
@@ -0,0 +1,31 @@
+package com.example.commercepaymentsystem.domain.cart.entity;
+
+
+import com.example.commercepaymentsystem.domain.product.entity.Product;
+import com.example.commercepaymentsystem.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "cart_item")
+public class CartItem extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private int quantity;
+}
