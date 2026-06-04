02511790437924198521b42b650569589bd77bdf
깡단 <yyb19920702@gmail.com>
2026-06-04 15:14:52 +0900

    fix : member controller,service 수정 기존 회원가입로직 member->auth로 이동했습니다

--- src/main/java/com/example/commercepaymentsystem/domain/member/controller/MemberController.java
+++ src/main/java/com/example/commercepaymentsystem/domain/member/controller/MemberController.java
@@ -1,34 +1,15 @@
 package com.example.commercepaymentsystem.domain.member.controller;
 
-import com.example.commercepaymentsystem.domain.member.dto.CreateMemberRequest;
-import com.example.commercepaymentsystem.domain.member.dto.CreateMemberResponse;
 import com.example.commercepaymentsystem.domain.member.service.MemberService;
-import com.example.commercepaymentsystem.global.response.ApiResponse;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/member")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
-
-    @PostMapping("/signup")
-    public ResponseEntity<ApiResponse<CreateMemberResponse>> signup(
-            @Valid @RequestBody CreateMemberRequest request
-    ) {
-        CreateMemberResponse result = memberService.signupMember(request);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(result, "회원가입성공"
-        ));
-    }
-
 
 }
